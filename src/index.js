c93f9a807d9de7d6498a0917cc9feadd9953f086
Anders Christensen <89982133+McAndersC@users.noreply.github.com>
2022-08-11 13:00:00 +0200

    Added json as file and endpoint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
+const renderProfileData = () => {
 
-var anders = 1;
-const peter = "TEST";
+    // fetch('https://api.github.com/users/PernilleMuhlig')
+    fetch('/data/profile.json')
+    .then((response) => response.json())
+    .then((profil) => {
 
-// fetch('https://api.github.com/users/PernilleMuhlig')
-fetch('/data/profile.json')
-.then((response) => response.json())
-.then((profil) => {
+        console.log(profil.avatar_url);
 
-    console.log(profil.avatar_url);
+        let imageContainer = document.querySelector('.profile-image')
+        let imageText = document.querySelector('.profile-text')
 
-    let imageContainer = document.querySelector('.profile-image')
-    let imageText = document.querySelector('.profile-text')
+        imageContainer.src = profil.avatar_url;
 
-    imageContainer.src = profil.avatar_url;
+        //imageText.innerHTML = `<b>${profil.login} er mit <i>navn</i></b>`
+        imageText.insertAdjacentHTML('beforebegin',  `<b>${profil.login} er mit <i>navn</i></b>`)
 
-    //imageText.innerHTML = `<b>${profil.login} er mit <i>navn</i></b>`
-    imageText.insertAdjacentHTML('beforebegin',  `<b>${profil.login} er mit <i>navn</i></b>`)
+        //imageText.textContent = profil.login;
+        
+        // imageText.innerHTML = '<b>' + profil.login + '</b>'
+        // '<b>' + profil.login + '</b>';
+        // imageText.textContent = profil.login;
+        
+        console.log('TEST', imageContainer.src, imageContainer.id)
 
-    //imageText.textContent = profil.login;
+    }).catch( (e) => console.log('Error', e))
+
+}
+// renderProfileData();
+
+const renderProfileList = (profileList) => {
+
+    let profileListContainer = document.querySelector('#profileList')
+
+    let userTmpl = (profile) => `
+        <div>
+            <h1>${profile.name} ${profile.lastname}</h1>
+            <img src="/assets/profileimages/${profile.avatar}" />
+        </div>
+    `
     
-    // imageText.innerHTML = '<b>' + profil.login + '</b>'
-    // '<b>' + profil.login + '</b>';
-    // imageText.textContent = profil.login;
-    
-    console.log('TEST', imageContainer.src, imageContainer.id)
+    profileList.map((profile) => {
 
-});
+        profileListContainer.insertAdjacentHTML('beforeend', userTmpl(profile));
+
+    })
+}
+
+const getProfileList = () => {
+
+    return fetch('/data/profileList.json')
+    .then((response) => response.json())
+}
+
+getProfileList().then((profileList) => renderProfileList(profileList))
